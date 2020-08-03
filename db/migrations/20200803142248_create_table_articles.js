22926040df0079984765b16a0fff45ece9ccdf14
Rosalyn Land <roz.s.land@gmail.com>
2020-08-03 14:35:41 +0100

    created articles table

---
+++ db/migrations/20200803142248_create_table_articles.js
@@ -0,0 +1,25 @@
+exports.up = function (knex) {
+  console.log("creating articles table...");
+  return knex.schema.createTable("articles", (articlesTable) => {
+    articlesTable.increments("article_id");
+    articlesTable.string("title").notNullable();
+    articlesTable.text("body").notNullable();
+    articlesTable.integer("votes").defaultTo(0);
+    articlesTable
+      .string("topic")
+      .references("slug")
+      .inTable("topics")
+      .notNullable();
+    articlesTable
+      .string("author")
+      .references("username")
+      .inTable("users")
+      .notNullable();
+    articlesTable.timestamp("created_at").defaultTo(knex.fn.now());
+  });
+};
+
+exports.down = function (knex) {
+  console.log("removing articles table...");
+  return knex.schema.dropTable("articles");
+};
