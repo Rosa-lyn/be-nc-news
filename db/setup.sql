b3bfd6bdb6e43b29aa803c2b95cf1e6f5f427951
Rosalyn Land <roz.s.land@gmail.com>
2020-08-03 13:01:16 +0100

    renames databases and created topics table migration

--- db/setup.sql
+++ db/setup.sql
@@ -1,5 +1,5 @@
-DROP DATABASE IF EXISTS nc_news_test;
-DROP DATABASE IF EXISTS nc_news;
+DROP DATABASE IF EXISTS northcoders_news_test;
+DROP DATABASE IF EXISTS northcoders_news;
 
-CREATE DATABASE nc_news_test;
-CREATE DATABASE nc_news;
+CREATE DATABASE northcoders_news_test;
+CREATE DATABASE northcoders_news;
