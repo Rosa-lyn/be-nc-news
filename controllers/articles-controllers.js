af7181a9a35df920531be906888a1ca325839a2f
Rosalyn Land <rosalyn.land@digitalworkroom.co.uk>
2022-03-08 17:37:39 +0000

    Add cors headers to topics and articles requests

--- controllers/articles-controllers.js
+++ controllers/articles-controllers.js
@@ -69,6 +69,14 @@ exports.sendArticles = (req, res, next) => {
     .then((resolvedPromises) => {
       const articles = resolvedPromises[0];
       const total_count = resolvedPromises[1];
+      res.setHeader("Access-Control-Allow-Origin", "*");
+      res.setHeader("Access-Control-Allow-Credentials", "true");
+      res.setHeader("Access-Control-Max-Age", "1800");
+      res.setHeader("Access-Control-Allow-Headers", "content-type");
+      res.setHeader(
+        "Access-Control-Allow-Methods",
+        "PUT, POST, GET, DELETE, PATCH, OPTIONS"
+      );
       res.send({
         total_count,
         articles,
--- controllers/topics-controllers.js
+++ controllers/topics-controllers.js
@@ -3,6 +3,14 @@ const { getTopics } = require("../models/topics-models");
 exports.sendTopics = (req, res, next) => {
   getTopics()
     .then((topics) => {
+      res.setHeader("Access-Control-Allow-Origin", "*");
+      res.setHeader("Access-Control-Allow-Credentials", "true");
+      res.setHeader("Access-Control-Max-Age", "1800");
+      res.setHeader("Access-Control-Allow-Headers", "content-type");
+      res.setHeader(
+        "Access-Control-Allow-Methods",
+        "PUT, POST, GET, DELETE, PATCH, OPTIONS"
+      );
       res.send({ topics });
     })
     .catch(next);
